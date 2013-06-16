b3f1339134967e7190e9639c706c7db563c94620
Bekt <bekt17@gmail.com>
2013-06-15 19:00:24 -0500

    2.3

---
+++ SkienaRevilla/src/Main_2_3.java
@@ -0,0 +1,36 @@
+//Status: AC
+//http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=30&page=show_problem&problem=991
+
+import java.util.Scanner;
+
+public class Main_2_3 {
+	static Scanner in = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int t = in.nextInt();
+		for (int i = 0; i < t; i++) {
+			System.out.println(solve(in.nextInt(), in.nextInt()));
+		}
+	}
+	
+	static int solve(int n, int p) {
+		int[] h = new int[p];
+		int hartals = 0;
+		
+		for (int i = 0; i < p; h[i++] = in.nextInt());
+		
+		for (int i = 1; i <= n; i++) {
+			if((i+1) % 7 == 0) {
+				i++;
+				continue;
+			}
+			for (int j = 0; j < p; j++) {
+				if(i % h[j] == 0) {
+					hartals++;
+					break;
+				}
+			}
+		}
+		return hartals;
+	}
+}
