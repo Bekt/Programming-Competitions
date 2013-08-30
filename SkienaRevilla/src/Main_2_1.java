f53f5c505b1591557c4a7d0f55e837113330f9af
Bekt <bekt17@gmail.com>
2013-08-30 02:37:15 -0500

    chapter 2

--- SkienaRevilla/src/Main_2_1.java
+++ SkienaRevilla/src/Main_2_1.java
@@ -1,5 +1,5 @@
-//Status: AC
-//http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=979
+// Problem: Jolly Jumpers
+// Status: AC
 
 import java.util.Scanner;
 import static java.lang.Math.abs;
@@ -14,26 +14,21 @@ public class Main_2_1 {
 	}
 
 	static String solve(int n) {
-		if(n == 1) {
-			in.nextInt();
-			return "Jolly";
-		}
-		
-		boolean[] values = new boolean[n];
-		int f, s = in.nextInt();
-		
-		for(int i=1; i<n; i++) {
-			f = s;
-			s = in.nextInt();
-			int diff = abs(s - f);
-			values[diff > values.length-1 ? 0 : diff] = true;
-		}
-		
-		for(int i=1; i<n; i++) {
-			if(!values[i]) {
-				return "Not jolly";
-			}
-		}			
-		return "Jolly";
+        boolean[] values = new boolean[n];
+        int f, s = in.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            f = s;
+            s = in.nextInt();
+            int diff = abs(s - f);
+            values[diff > values.length - 1 ? 0 : diff] = true;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (!values[i]) {
+                return "Not jolly";
+            }
+        }
+        return "Jolly";
 	}	
 }
--- SkienaRevilla/src/Main_2_3.java
+++ SkienaRevilla/src/Main_2_3.java
@@ -1,5 +1,5 @@
-//Status: AC
-//http://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=30&page=show_problem&problem=991
+// Problem: Hartals
+// Status: AC
 
 import java.util.Scanner;
 
@@ -7,30 +7,30 @@ public class Main_2_3 {
 	static Scanner in = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		int t = in.nextInt();
-		for (int i = 0; i < t; i++) {
-			System.out.println(solve(in.nextInt(), in.nextInt()));
-		}
-	}
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            System.out.println(solve(in.nextInt(), in.nextInt()));
+        }
+    }
 	
 	static int solve(int n, int p) {
-		int[] h = new int[p];
-		int hartals = 0;
-		
-		for (int i = 0; i < p; h[i++] = in.nextInt());
-		
-		for (int i = 1; i <= n; i++) {
-			if((i+1) % 7 == 0) {
-				i++;
-				continue;
-			}
-			for (int j = 0; j < p; j++) {
-				if(i % h[j] == 0) {
-					hartals++;
-					break;
-				}
-			}
-		}
-		return hartals;
-	}
+        int[] h = new int[p];
+        int hartals = 0;
+
+        for (int i = 0; i < p; h[i++] = in.nextInt());
+
+        for (int i = 1; i <= n; i++) {
+            if ((i + 1) % 7 == 0) {
+                i++;
+                continue;
+            }
+            for (int j = 0; j < p; j++) {
+                if (i % h[j] == 0) {
+                    hartals++;
+                    break;
+                }
+            }
+        }
+        return hartals;
+    }
 }
