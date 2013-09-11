336d22bb44a3e5204b4d82ce91771beda43f682d
Bekt <bekt17@gmail.com>
2013-09-10 21:57:33 -0500

    4.5

--- SkienaRevilla/src/Main_4_5.java
+++ SkienaRevilla/src/Main_4_5.java
@@ -1,48 +1,47 @@
-//Status: AC
+// Problem: Shoemaker's problem
+// Status: AC
 
 import java.util.*;
 
 public class Main_4_5 {
-	static Scanner in = new Scanner(System.in);
-	
-	public static void main(String[] args) {
-		int t = in.nextInt();
-		for (int i = 0; i < t; i++) {
-			solve();
-			if (i < t-1) {
-				System.out.println();				
-			}
-		}
-	}
-	
-	static void solve() {
-		int n = in.nextInt();
-		List<Num> nums = new ArrayList<Num>();
-		for (int i = 0; i < n; i++) {
-			nums.add(new Num(in.nextInt(), in.nextInt(), i+1));
-		}
-		Collections.sort(nums);
-		System.out.println(nums.toString().replaceAll("[\\[\\],]",  ""));
-	}
-	
-	static class Num implements Comparable<Num> {
-		int index;
-		double r;
-		public Num(int t, int s, int index) {
-			this.index = index;
-			this.r = (double) s / t;
-		}
-		
-		@Override
-		public int compareTo(Num obj) {
-			if(r > obj.r) return -1;
-			if(r < obj.r) return 1;
-			if(index < obj.index) return -1;
-			return 1;
-		}
-		
-		public String toString() {
-			return String.valueOf(index);
-		}
-	}
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = in.nextInt();
+            List<Num> nums = new ArrayList<Num>();
+            for (int j = 1; j <= n; j++) {
+                nums.add(new Num(in.nextInt(), in.nextInt(), j));
+            }
+            Collections.sort(nums);
+            System.out.println(nums.toString().replaceAll("[\\[\\],]", ""));
+
+            if (i < t - 1) {
+                System.out.println();
+            }
+        }
+    }
+
+    static class Num implements Comparable<Num> {
+        int index;
+        double r;
+
+        public Num(int t, int s, int index) {
+            this.index = index;
+            this.r = (double) s / t;
+        }
+
+        @Override
+        public int compareTo(Num obj) {
+            if (r > obj.r) return -1;
+            if (r < obj.r) return 1;
+            if (index < obj.index) return -1;
+            return 1;
+        }
+
+        public String toString() {
+            return String.valueOf(index);
+        }
+    }
 }
