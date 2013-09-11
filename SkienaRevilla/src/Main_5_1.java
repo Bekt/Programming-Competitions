6e8d765554975d2e16306032a8fdce12889e5aac
Bekt <bekt17@gmail.com>
2013-09-11 18:35:40 -0500

    Chapter 5

--- SkienaRevilla/src/Main_5_1.java
+++ SkienaRevilla/src/Main_5_1.java
@@ -1,41 +1,34 @@
+// Problem: Primary Arithmetic
 // Status: AC
 
 import java.util.Scanner;
 
 public class Main_5_1 {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		long x, y;
-		while ((x = in.nextLong()) + (y = in.nextLong()) > 0) {
-			int count = getCount(x, y);
-			String output;
-			switch (count) {
-				case 0:
-					output = "No carry operation.";
-					break;
-				case 1:
-					output = count + " carry operation.";
-					break;
-				default:
-					output = count + " carry operations.";
-					break;
-			}
-			System.out.println(output);
-		}
-	}
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        long x, y;
+        while ((x = in.nextLong()) + (y = in.nextLong()) > 0) {
+            int count = getCount(x, y);
+            System.out.println(count == 0
+                    ? "No carry operation."
+                    : count == 1
+                        ? "1 carry operation."
+                        : count + " carry operations.");
+        }
+    }
 
-	static int getCount(long x, long y) {
-		int count = 0;
-		boolean isCarry = false;
-		while (x > 0 || y > 0) {
-			long n = (x % 10) + (y % 10) + (isCarry ? 1 : 0);
-			isCarry = n > 9;
-			count += isCarry ? 1 : 0;
-			x /= 10;
-			y /= 10;
-		}
-		return count;
-	}
+    static int getCount(long x, long y) {
+        int count = 0;
+        boolean isCarry = false;
+        while (x > 0 || y > 0) {
+            long n = (x % 10) + (y % 10) + (isCarry ? 1 : 0);
+            isCarry = n > 9;
+            count += isCarry ? 1 : 0;
+            x /= 10;
+            y /= 10;
+        }
+        return count;
+    }
 
 }
--- SkienaRevilla/src/Main_5_4.java
+++ SkienaRevilla/src/Main_5_4.java
@@ -1,29 +1,23 @@
-//Status: AC
+// Problem: Ones
+// Status: AC
 
 import java.util.Scanner;
 
 public class Main_5_4 {
-	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int[] data = new int[10001];
-		
-		for (int i = 1; i < data.length; i++) {
-			if((i % 2) == 0 || (i % 5) == 0) continue;
-			data[i] = getCount(i);
-		}
-		
-		while(in.hasNextInt()) {
-			System.out.println(data[in.nextInt()]);
-		}
-	}
-	
-	static int getCount(int n) {
-		int curr = 1, count = 1;
-		while((curr % n) != 0) {
-			curr = (curr * 10 + 1) % n;
-			count++;
-		}
-		return count;
-	}
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            System.out.println(getCount(in.nextInt()));
+        }
+    }
+
+    static int getCount(int n) {
+        int curr = 1, count = 1;
+        while ((curr % n) != 0) {
+            curr = (curr * 10 + 1) % n;
+            count++;
+        }
+        return count;
+    }
 }
