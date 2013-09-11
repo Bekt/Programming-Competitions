6b359ef3758e2752371c5a9c5f9e629bfe30cb90
Bekt <bekt17@gmail.com>
2013-09-10 22:17:52 -0500

    4.1

--- SkienaRevilla/src/Main_4_1.java
+++ SkienaRevilla/src/Main_4_1.java
@@ -1,37 +1,35 @@
-//Chapter 4: 4.6.1 Vito’s Family
-//Status: WA
+// Vito's Family
+// Status: WA
 
 import java.util.*;
-import static java.lang.Math.*;
+import static java.lang.Math.abs;
 
 public class Main_4_1 {
-	static Scanner in;
-	public static void main(String[] args) {
-		in = new Scanner(System.in);
-		int n = in.nextInt();
-		for (int i = 0; i < n; i++) {
-			solve();
-		}
-	}
-	
-	static void solve() {
-		int r = in.nextInt();
-		Set<Integer> set = new HashSet<Integer>();
-		int sum = 0, counter = 0, diff = 0;
-		for (int i = 0; i < r; i++) {
-			int s = in.nextInt();
-			if(!set.contains(s)) {
-				set.add(s);
-				sum += s;
-				counter++;
-			}
-		}
-		int median = (int) ((double)sum / counter + 0.5);
-		
-		Iterator<Integer> iter = set.iterator();
-		while(iter.hasNext()) {
-			diff += abs(median - iter.next());
-		}
-		System.out.println(diff);
-	}
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            int r = in.nextInt();
+            List<Integer> relatives = new ArrayList<Integer>();
+            for (int j = 0; j < r; j++) {
+                relatives.add(in.nextInt());
+            }
+            System.out.println(solve(relatives));
+        }
+    }
+
+    static int solve(List<Integer> relatives) {
+        int median = getMedian(relatives), diff = 0;
+        for (int i : relatives) {
+            diff += abs(median - i);
+        }
+        return diff;
+    }
+
+    static int getMedian(List<Integer> numbers) {
+        Collections.sort(numbers);
+        int size = numbers.size();
+        return size % 2 == 0 ? numbers.get(size / 2 - 1) : numbers.get(size / 2);
+    }
 }
