bd34357eb78595a1f84096d23bcb69301c3c709f
Bekt <bekt17@gmail.com>
2013-12-10 02:18:04 -0600

    12.1

---
+++ SkienaRevilla/src/Main_12_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import static java.lang.Math.*;
+
+public class Main_12_1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n;
+        while ((n = in.nextInt()) != 0) {
+            int sq = (int) ceil(sqrt(n));
+            int coord = sq * sq - sq + 1;
+            int y = n > coord ? sq - n + coord : sq;
+            int x = n > coord ? sq : sq - coord + n;
+            System.out.println(sq % 2 == 0 ? (x + " " + y) : (y + " " + x));
+        }
+    }
+
+}
