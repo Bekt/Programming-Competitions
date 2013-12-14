388af7975294d438cd1c237147fd11d7b48bc921
Bekt <bekt17@gmail.com>
2013-12-13 19:02:36 -0600

    7.5

--- SkienaRevilla/src/Main_7_5.java
+++ SkienaRevilla/src/Main_7_5.java
@@ -1,50 +1,49 @@
-import java.util.Arrays;
-import java.util.HashSet;
+// Problem: Summation of Four Primes
+// Status: AC
+
 import java.util.Scanner;
-import java.util.Set;
-import static java.lang.Math.sqrt;
 
 public class Main_7_5 {
 
-    static Set<Integer> primes = new HashSet<Integer>();
+    static boolean[] notPrime = calcPrimes(10000000);
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        calcPrimes(10000000);
         while (in.hasNext()) {
             solve(in.nextInt());
         }
     }
 
     static void solve(int n) {
-        int sum = n % 2 == 0 ? 4 : 5;
-        for (int i : primes) {
-            if (primes.contains(n - (sum + i))) {
-                System.out.print((n - (sum + i)) + " " + i + " ");
-                System.out.println(2 + " " + (n % 2 == 0 ? 2 : 3));
-                return;
-            }
-        }
-        System.out.println("Impossible.");
-    }
-
-    static void calcPrimes(int n) {
-        boolean[] isPrime = new boolean[n + 1];
-        Arrays.fill(isPrime, true);
-        double sq = sqrt(isPrime.length);
-        for (int i = 2; i <= sq; i++) {
-            if (isPrime[i]) {
-                for (int j = i + i; j < isPrime.length; j += i) {
-                    isPrime[j] = false;
+        if (n < 8) {
+            System.out.println("Impossible.");
+        } else {
+            System.out.print("2 " + (n % 2 == 0 ? "2 " : "3 "));
+            n -= (4 + (n % 2));
+            for (int i = 2; n -i >= 2; i++) {
+                if (!notPrime[i] && !notPrime[n - i]) {
+                    System.out.println(i + " " + (n - i));
+                    return;
                 }
             }
         }
-        primes.add(2);
-        for (int i = 3; i < isPrime.length; i += 2) {
-            if (isPrime[i]) {
-                primes.add(i);
+    }
+
+    static boolean[] calcPrimes(int n) {
+        boolean[] notPrime = new boolean[n + 1];
+        notPrime[0] = true;
+        notPrime[1] = true;
+        for (int i = 4; i <= n; i += 2) {
+            notPrime[i] = true;
+        }
+        for (int i = 3; i * i <= n; i += 2) {
+            if (!notPrime[i]) {
+                for (int j = i * i; j <=n; j += i) {
+                    notPrime[j] = true;
+                }
             }
         }
+        return notPrime;
     }
 
 }
